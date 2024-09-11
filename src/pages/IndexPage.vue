6824f5972ef3f260fedf6d13b7d6527ce0f96f81
Joao Devson Mucavel <joao.mucavel@ideiasdinamicas.com>
2024-09-11 12:21:32 +0200

    Setting up Repository

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -4,12 +4,20 @@
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
-    >
+    />
   </q-page>
 </template>
 
 <script setup>
+import { useLocalStorage, useMouse, usePreferredDark } from "@vueuse/core";
+
+const { x, y } = useMouse();
+
+window.addEventListener("mousemove", () => {
+  console.log("Coordenadas do Mouse: ", x.value, y.value);
+});
+
 defineOptions({
-  name: 'IndexPage'
+  name: "IndexPage",
 });
 </script>
